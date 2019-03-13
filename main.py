b32d1b019dcde57081194763bf956a1117feb595
caiyueliang <393900414@qq.com>
2019-03-13 08:14:56 +0800

    update

---
+++ main.py
@@ -0,0 +1,32 @@
+# encoding: utf-8
+import gym
+
+
+# 手推车杆
+def cart_pole():
+    env = gym.make('CartPole-v0')
+    env.reset()
+    for _ in range(1000):
+        env.render()
+        env.step(env.action_space.sample())     # take a random action
+
+
+# 手推车杆
+def cart_pole_1():
+    env = gym.make('CartPole-v0')
+    observation = env.reset()
+    for t in range(1000):
+        env.render()
+        print('[observation]', observation)
+        action = env.action_space.sample()
+        print('[observation] action', action)
+        observation, reward, done, info = env.step(action)
+        print('[observation]:', observation, reward, done, info)
+        # if done:
+        #     print("Episode finished after {} timesteps".format(t + 1))
+        #     break
+
+
+if __name__ == "__main__":
+    # cart_pole()
+    cart_pole_1()
