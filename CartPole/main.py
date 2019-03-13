2f5effc84925595bf931d8a8799d67d556278bdf
caiyueliang <393900414@qq.com>
2019-03-13 11:59:47 +0800

    update

--- CartPole/main.py
+++ CartPole/main.py
@@ -77,27 +77,30 @@ def cart_pole_2():
         ep_r = 0
 
         while True:  # 训练没有结束的时候循环
-            env.render()                                                # 刷新环境
-            action = RL.choose_action(observation)                      # 根据状态选择行为
-            observation_next, reward, done, info = env.step(action)     # 环境模型 采用行为，获得下个状态，和潜在的奖励
+            env.render()                                                        # 刷新环境
+            action = RL.choose_action(observation)                              # 根据状态选择行为
+            observation_next, reward, done, info = env.step(action)             # 环境模型 采用行为，获得下个状态，和潜在的奖励
 
-            x, x_dot, theta, theat_dot = observation_next               # 这里拆分了 状态值 ，里面有四个参数
+            x, x_dot, theta, theat_dot = observation_next                       # 这里拆分了 状态值 ，里面有四个参数
+            print('x %d, x_dot %d, theta %d, theat_dot %d, reward %d, done %s' %
+                  (x, x_dot, theta, theat_dot, reward, done))
             # 这里用了，x 和theta的限度值 来判断奖励的幅度，当然也可以gym自带的 ，但是这个效率据说比较高
             reward1 = (env.x_threshold - abs(x)) / env.x_threshold - 0.8
             reward2 = (env.theta_threshold_radians - abs(theta)) / env.theta_threshold_radians - 0.5
-            reward = reward1 + reward2                                  # 这里将奖励综合
-
+            reward = reward1 + reward2                                          # 将奖励综合
+            print('reward1 %d, reward2 %d, reward3 %d' % (reward1, reward2, reward))
             RL.store_transition(observation, action, reward, observation_next)  # 先存储到记忆库
+
             ep_r += reward          # 这里只是为了观察奖励值是否依据实际情况变化，来方便判断模型的正确性
             if steps > 1000:        # 这里一开始先不学习，先积累奖励
                 RL.learn()
             if done:                # 这里判断的是回合结束，显示奖励积累值，你可以看到每回合奖励的变化，来判定这样一连串行为的结果好不好
                 print('episode :', episode,
-                      'ep_r:', round(ep_r, 2),
+                      'reward:', round(ep_r, 2),
                       "RL's epsilon", round(RL.epsilon, 3))
                 break
 
-            observation = observation_next  # 跟新状态
+            observation = observation_next          # 更新状态
             steps += 1
     RL.plot_cost()  # 训练结束后来观察我们的cost
 
